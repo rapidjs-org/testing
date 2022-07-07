506501fb5801511e8f071a1f8cc58561fb320b79
t-ski <t.schiepanski@gmx.de>
2022-07-08 01:03:45 +0200

    Implement print module

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,11 @@
  */
 
 
-console.log("Hello world!");
+import * as print from "./print";
+
+
+print.fileName("test.js");
+print.badge("TEST SUITE A", 245, 122, 154);
+print.badge("TEST SUITE B", 245, 225, 174);
+print.success("Success in test 1");
+print.failure("Failure in test 2");
---
+++ src/print.ts
@@ -0,0 +1,40 @@
+
+// TODO: Intercept app log
+
+
+enum Layer {
+    FG,
+    BG
+}
+
+
+function log(message: string) {
+    process.stdout.write(`${message}\n`);
+}
+
+function colorFrom(layer: Layer, r: number, g: number, b: number|string): string {
+    return `${(layer === Layer.FG) ? "38;2;" : "48;2;"}${r};${g};${b}`;
+}
+
+function styleWrapStr(str: string, styles: string|string[]): string {
+    styles = !Array.isArray(styles) ? [ styles ]: styles;
+
+    return `${styles.map((style: string) => `\x1b[${style}m`).join("")}${str}\x1b[0m`;
+}
+
+
+export function fileName(message: string) {
+    log(styleWrapStr(`• ${message}`, [ colorFrom(Layer.FG, 225, 225, 235) ]));
+}
+
+export function badge(message: string, r: number, g: number, b: number) {
+    log(styleWrapStr(` ${message} `, [ "1", colorFrom(Layer.BG, r, g, b) ].concat(((r + g + b) < 550) ? [ "97" ] : [])));
+}
+
+export function success(message: string) {
+    log(styleWrapStr(`✔ ${message} `, colorFrom(Layer.FG, 25, 225, 125)));
+}
+
+export function failure(message: string) {
+    log(styleWrapStr(`✘ ${message} `, colorFrom(Layer.FG, 225, 25, 125)));
+}
